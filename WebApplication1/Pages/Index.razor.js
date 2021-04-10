e75aa4c50177c4efea154aadd1608283abcc307e
Mithun Ashwathappa <mithun.ta@gmail.com>
2021-04-10 15:13:35 -0400

    Add project files.

---
+++ WebApplication1/Pages/Index.razor.js
@@ -0,0 +1,25 @@
+﻿
+
+window.onload = function () {
+
+    alert("here");
+    const video = document.getElementById('myVideo');
+
+    video.addEventListener('pause', (event) => {
+        //document.getElementById("video_overlays").style.visibility = "visible";
+        let time = video.currentTime;
+        alert("pause");
+    });
+    video.addEventListener('play', (event) => {
+        alert("play");
+    });
+}
+
+
+window.pauseVideo = function () {
+    var video = document.getElementById('myvideo');
+    let time = video.currentTime;
+    alert(time);
+
+    return time;
+}
